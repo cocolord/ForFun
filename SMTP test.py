e8695cdb31dfe9999c7fcb7a28053ebfb88166bb
cocolord <cocolord@users.noreply.github.com>
2017-03-27 21:17:56 +0800

    Update SMTP test.py

--- SMTP test.py
+++ SMTP test.py
@@ -1,16 +1,31 @@
 # -*- coding: utf-8 -*-
 import time
+from email import encoders
 import smtplib
-from email import*
 from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
 
-_user = "any@163.com"
-_pwd = "**"
-_to = "who@163.com"
-msg = MIMEText('','html')#content here
+_user = "he@163.com"
+_pwd = "&&"
+_to = "elder@163.com"
+
+msg = MIMEMultipart()
 msg['Subject'] = u'你好'.encode('gbk')
 msg['From'] = u'麦克雷'.encode('gbk')
 msg['To'] = _to
+#attach some content
+msg.attach(MIMEText('test'))
+
+# add file:
+with open('/home/maxtod/Documents/QQ.png', 'rb') as f:
+    mime = MIMEBase('image', 'png', filename='test.png')
+    # mime.add_header('Content-Disposition', 'attachment', filename='test.png')
+    # mime.add_header('Content-ID', '<0>')
+    # mime.add_header('X-Attachment-Id', '0')
+    mime.set_payload(f.read())
+    encoders.encode_base64(mime)
+    msg.attach(mime)
 
 s = smtplib.SMTP("smtp.163.com",timeout=300)
 s.starttls()
