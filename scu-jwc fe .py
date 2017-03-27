0c5cfbfdf22773d129eaf04e6d9cb966ed924eac
cocolord <cocolord@users.noreply.github.com>
2017-03-27 20:58:14 +0800

    Create scu-jwc fe .py

---
+++ scu-jwc fe .py
@@ -0,0 +1,20 @@
+import urllib
+import urllib2
+import cookielib
+from bs4 import BeautifulSoup
+import re
+filename = 'cookie.txt'
+cookie = cookielib.MozillaCookieJar(filename)
+handler = urllib2.HTTPCookieProcessor(cookie)
+opener = urllib2.build_opener(handler)
+postdata = urllib.urlencode({
+    'zjh':"2015141052011",
+    'mm':"xxx"
+})
+url = 'http://202.115.47.141/loginAction.do'
+result = opener.open(url,postdata)
+cookie.save(ignore_discard=True, ignore_expires=True)
+result = opener.open('http://202.115.47.141/bxqcjcxAction.do')
+soup = BeautifulSoup(result,'lxml')
+print soup
+print(soup.find_all("tr","odd"))
