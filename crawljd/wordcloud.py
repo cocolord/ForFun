32f7b8963f287f96a8eba2f50516afb85e6d9917
cicolord <cicolord@outlook.com>
2018-11-04 23:41:34 +0800

    Modify readme

--- crawljd/wordcloud.py
+++ crawljd/wordcloud.py
@@ -1,17 +1,24 @@
 from scipy.misc import imread
 import matplotlib.pyplot as plt
 import jieba
-from wordcloud import WordCloud
-#import wordcloud
+import codecs
+# from wordcloud import WordCloud
+import wordcloud
 
-f = open('/home/dong/Documents/iphonexcomments.txt','rt')
+f = open('iphonexcomments.txt','rt')
+stoplist = {}.fromkeys([content.strip() for content in open("./stop_word.txt", encoding='UTF-8')])
 text = f.read()
 #本地存放字体
-cut_text = ' '.join(jieba.lcut(text))
-print(cut_text)
-color_mask = imread("/home/dong/Downloads/iphone.png")
-cloud = WordCloud(
-    font_path='/home/dong/Downloads/fzmw.ttf',
+cut_text = ' '.join(jieba.lcut(text,cut_all= False))
+cut_text = [word for word in list(cut_text) if word not in stoplist]
+
+color_mask = imread("iphone.png")
+file_write = codecs.open('./cut_text.txt', 'w', 'UTF-8')
+for i in range(len(cut_text)):
+    file_write.write(str(cut_text[i]) + '\n')
+print("Write operation succeeded")
+cloud = wordcloud.WordCloud(
+    font_path='fzmw.ttf',
     background_color='white',
     mask=color_mask,
     max_words=200,
